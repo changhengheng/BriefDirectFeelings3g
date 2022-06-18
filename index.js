522f3bf77ad115037afb5f08ce67a6f0c622fbfa
changhengheng <321121653@qq.com>
2022-06-18 13:35:02 +0800

    update

---
+++ index.js
@@ -0,0 +1,34 @@
+module.exports = {
+  plugins: [
+    ['@vuepress/blog', {
+      directories: [
+        {
+          id: 'post',
+          dirname: '_posts',
+          path: '/',
+          pagination: {
+            perPagePosts: 2,
+          },
+        }, {
+          id: "dynamicactivities",
+          dirname: '_DynamicActivities',
+          path: '/_dynamicActivities/',
+        },
+      ],
+      frontmatters: [
+        {
+          id: "tag",
+          keys: ['tag', 'tags'],
+          path: '/tag/',
+          layout: 'Tag',
+          frontmatter: { title: 'Tag' },
+          itemlayout: 'Tag',
+          pagination: {
+            perPagePosts: 3
+          }
+        },
+
+      ]
+    }],
+  ],
+}
